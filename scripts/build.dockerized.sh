6d678b32c3a60b9e0fbde3d8eb33da93ee4d4127
Danny Berger <dpb587@gmail.com>
2021-01-26 05:23:05 -0700

    Another fix for stricter build steps
    Broken since d6dc63cb569bef320eb5de3bfb71a9417e892bbf

--- scripts/build.dockerized.sh
+++ scripts/build.dockerized.sh
@@ -6,7 +6,9 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 docker build -t dpb587/gget:build build/docker/build
 docker run --rm \
-  --volume=$PWD:/root \
+  --volume="${PWD}":/root \
   --workdir=/root \
+  --user=$( id -u "${USER}" ):$( id -g "${USER}" ) \
+  --env=GOCACHE=/tmp/.cache/go-build \
   dpb587/gget:build \
   ./scripts/build.local.sh "$@"
